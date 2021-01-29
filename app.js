b656a0e143030629e34f855ca316b742c3e035d8
joshuamv <joshuajmv@me.com>
2021-01-29 15:43:52 +0200

    ok

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 
 //////////////// global vars /////////////////
 
+//light
+let day = true;
+let night = false;
 
+//animals
+let crab = false;
+let fish = false;
+let dayJellyfish = false;
+let nightJellyfish = false;
+let fireflies = false;
+
+//badges
+let sunrise = false;
+let sunset = false;
+let rainbow = false;
+let whale = false;
+
+//pointer states
+let regularCursor = true;
+let waitCursor = false;
+let blackCursor = false;
+
+//game states
+let loadingState = false;
+let tutorialState = false;
+let gameplayState = false;
+let gameoverState = false;
+let badgeState = false;
+let gamewonState = false;
 
 //////////////// run when html loads /////////////////
 
 $(document).ready(function() {
-
+  
 }
 
 //////////////// functions /////////////////
+
+function functionName() {
+
+}
